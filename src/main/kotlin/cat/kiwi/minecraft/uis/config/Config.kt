41fcc6da8b987054a7beb4939d93e3003f632e2e
moeKiwiSAMA <me@kiwi.cat>
2022-10-12 17:33:29 +0800

    default config

--- src/main/kotlin/cat/kiwi/minecraft/uis/config/Config.kt
+++ src/main/kotlin/cat/kiwi/minecraft/uis/config/Config.kt
@@ -8,66 +8,148 @@ object Config {
 
     var enableTax: Boolean = false
     var taxRate: Double = 0.0
+    var enablePapi = false
     lateinit var taxAccountUUID: String
 
     private val logger = UltimateInventoryShopPlugin.instance.logger
 
     fun readConfig(instance: UltimateInventoryShopPlugin) {
         instance.saveDefaultConfig()
+        val unFindConfig = mutableListOf<String>()
+        //  sqlError: "数据库连接失败"
+        //  materialLoadError: "材料读取失败"
+        //  taxAccountError: "税收账户配置错误"
+        //  prefix: "§6§l[全球商店] §f"
+        //  noPermission: "§c你没有权限执行此操作"
+        //  reloadLang: "§a语言文件已重载"
+        //  taxRateInfo: "§2当前税率: §e"
+        //  uisTitle: "§2全球商店"
+        //  currentGoods: "§9当前商品"
+        //  myGoods: "§9我出售的"
+        //  myGoodsBeenSold: "§9我出售的(已卖出)"
+        //  pageIndexName: "§9当前页码: §9"
+        //  previousPage: "§9上一页"
+        //  nextPage: "§9下一页"
+        //  seller: "§6卖家 §f"
+        //  buyer: "§6买家 §6"
+        //  price: "§2价格 §6"
+        //  afterTax: "§2税后价格: §f"
+        //  emptyInMainHand: "§c手上没有东西"
+        //  sellUsage: "§2/uis sell <价格> <描述>"
+        //  invalidItem: "§c购买失败，物品可能已经下架"
+        //  sellSuc: "§2出售成功"
+        //  sellFail: "§c出售失败"
+        //  playerNotFound: "§c未找到玩家"
+        //  buyFail: "§c购买失败，商品可能已被他人购买"
+        //  buySuc: "§2购买成功"
+        //  noEnoughMoney: "§2余额不足"
+        //  invFull: "§c物品栏已满"
+        //  redeemSuc: "§2赎回成功"
+        //  redeemFail: "§c赎回失败"
+        //  cannotBuySelfGoods: "§c不能购买自己的商品"
+        //  notBelongToYou: "§c商品不属于你"
+        //  sellPriceError: "§c价格不能小于0"
+        //  helpMessage:
+        //    - '§6§l[全球商店帮助]'
+        //    - '§6§l/uis sell <价格> <描述(可选)> §2出售手上的物品'
+        //    - '§6§l/uis open §2打开商店'
+        //    - '§6§l/uis open <玩家> §2查询某个玩家出售的物品'
+        //    - '§6§l/uis help §2查看帮助'
+        //    - '§6§l/uis reload §2重载配置'
+        //    - '§6§l/uis tax §2查看税率'
         try {
             debug = instance.config.getBoolean("debug")
             tablePrefix = instance.config.getString("dataSource.prefix")!!
             enableTax = instance.config.getBoolean("enableTax")
             taxAccountUUID = instance.config.getString("taxAccountUUID")!!
             taxRate = instance.config.getDouble("taxRate")
-            Lang.prefix = instance.config.getString("lang.prefix")!!
-            Lang.uisTitle = instance.config.getString("lang.uisTitle")!!
-            Lang.currentGoods = instance.config.getString("lang.currentGoods")!!
-            Lang.myGoods = instance.config.getString("lang.myGoods")!!
-            Lang.myGoodsBeenSold = instance.config.getString("lang.myGoodsBeenSold")!!
-            Lang.pageIndexName = instance.config.getString("lang.pageIndexName")!!
-            Lang.previousPage = instance.config.getString("lang.previousPage")!!
-            Lang.nextPage = instance.config.getString("lang.nextPage")!!
-            Lang.emptyInMainHand = instance.config.getString("lang.emptyInMainHand")!!
-            Lang.sellUsage = instance.config.getString("lang.sellUsage")!!
-            Lang.materialLoadError = instance.config.getString("lang.materialLoadError")!!
-            Lang.price = instance.config.getString("lang.price")!!
-            Lang.sellSuc = instance.config.getString("lang.sellSuc")!!
-            Lang.sellFail = instance.config.getString("lang.sellFail")!!
-            Lang.sqlError = instance.config.getString("lang.sqlError")!!
-            Lang.seller = instance.config.getString("lang.seller")!!
-            Lang.buyer = instance.config.getString("lang.buyer")!!
-            Lang.playerNotFound = instance.config.getString("lang.playerNotFound")!!
-            Lang.buyFail = instance.config.getString("lang.buyFail")!!
-            Lang.invalidItem = instance.config.getString("lang.invalidItem")!!
-            Lang.buySuc = instance.config.getString("lang.buySuc")!!
-            Lang.noEnoughMoney = instance.config.getString("lang.noEnoughMoney")!!
-            Lang.invFull = instance.config.getString("lang.invFull")!!
-            Lang.taxAccountError = instance.config.getString("lang.taxAccountError")!!
-            Lang.afterTax = instance.config.getString("lang.afterTax")!!
-            Lang.redeemSuc = instance.config.getString("lang.redeemSuc")!!
-            Lang.redeemFail = instance.config.getString("lang.redeemFail")!!
-            Lang.cannotBuySelfGoods = instance.config.getString("lang.cannotBuySelfGoods")!!
-            Lang.notBelongToYou = instance.config.getString("lang.notBelongToYou")!!
-            Lang.sellPriceError = instance.config.getString("lang.sellPriceError")!!
-            Lang.helpMessage = instance.config.getList("lang.helpMessage")!!.joinToString("\n")
-            Lang.noPermission = instance.config.getString("lang.noPermission")!!
-            Lang.reloadLang = instance.config.getString("lang.reloadLang")!!
-            Lang.taxRateInfo = instance.config.getString("lang.taxRateInfo")!!
+            enablePapi = instance.config.getBoolean("enablePapi")
+            Lang.prefix =
+                instance.config.getString("lang.prefix") ?: "§6§l[全球商店] §f".also { unFindConfig.add("lang.prefix") }
+            Lang.uisTitle =
+                instance.config.getString("lang.uisTitle") ?: "§2全球商店".also { unFindConfig.add("lang.uisTitle") }
+            Lang.currentGoods = instance.config.getString("lang.currentGoods")
+                ?: "§9当前商品".also { unFindConfig.add("lang.currentGoods") }
+            Lang.myGoods =
+                instance.config.getString("lang.myGoods") ?: "§9我出售的".also { unFindConfig.add("lang.myGoods") }
+            Lang.myGoodsBeenSold = instance.config.getString("lang.myGoodsBeenSold")
+                ?: "§9我出售的(已售出)".also { unFindConfig.add("lang.myGoodsBeenSold") }
+            Lang.pageIndexName = instance.config.getString("lang.pageIndexName")
+                ?: "§9当前页码: §9".also { unFindConfig.add("lang.pageIndexName") }
+            Lang.previousPage = instance.config.getString("lang.previousPage")
+                ?: "§9上一页".also { unFindConfig.add("lang.previousPage") }
+            Lang.nextPage =
+                instance.config.getString("lang.nextPage") ?: "§9下一页".also { unFindConfig.add("lang.nextPage") }
+            Lang.emptyInMainHand = instance.config.getString("lang.emptyInMainHand")
+                ?: "§c手上没有东西".also { unFindConfig.add("lang.emptyInMainHand") }
+            Lang.sellUsage = instance.config.getString("lang.sellUsage")
+                ?: "§2/uis sell <价格> <描述>".also { unFindConfig.add("lang.sellUsage") }
+            Lang.materialLoadError = instance.config.getString("lang.materialLoadError")
+                ?: "§c物品加载失败".also { unFindConfig.add("lang.materialLoadError") }
+            Lang.price = instance.config.getString("lang.price") ?: "§2价格 §6".also { unFindConfig.add("lang.price") }
+            Lang.sellSuc =
+                instance.config.getString("lang.sellSuc") ?: "§2出售成功".also { unFindConfig.add("lang.sellSuc") }
+            Lang.sellFail =
+                instance.config.getString("lang.sellFail") ?: "§c出售失败".also { unFindConfig.add("lang.sellFail") }
+            Lang.sqlError =
+                instance.config.getString("lang.sqlError") ?: "§c数据库错误".also { unFindConfig.add("lang.sqlError") }
+            Lang.seller =
+                instance.config.getString("lang.seller") ?: "§2卖家 §6".also { unFindConfig.add("lang.seller") }
+            Lang.buyer = instance.config.getString("lang.buyer") ?: "§2买家 §6".also { unFindConfig.add("lang.buyer") }
+            Lang.playerNotFound = instance.config.getString("lang.playerNotFound")
+                ?: "§c未找到玩家".also { unFindConfig.add("lang.playerNotFound") }
+            Lang.buyFail = instance.config.getString("lang.buyFail")
+                ?: "§c购买失败，商品可能已被他人购买".also { unFindConfig.add("lang.buyFail") }
+            Lang.invalidItem = instance.config.getString("lang.invalidItem")
+                ?: "§c无效的物品".also { unFindConfig.add("lang.invalidItem") }
+            Lang.buySuc =
+                instance.config.getString("lang.buySuc") ?: "§2购买成功".also { unFindConfig.add("lang.buySuc") }
+            Lang.noEnoughMoney = instance.config.getString("lang.noEnoughMoney")
+                ?: "§c你的余额不足".also { unFindConfig.add("lang.noEnoughMoney") }
+            Lang.invFull =
+                instance.config.getString("lang.invFull") ?: "§c你的背包已满".also { unFindConfig.add("lang.invFull") }
+            Lang.taxAccountError = instance.config.getString("lang.taxAccountError")
+                ?: "§c税收账户错误".also { unFindConfig.add("lang.taxAccountError") }
+            Lang.afterTax =
+                instance.config.getString("lang.afterTax") ?: "§2税后价格 §6".also { unFindConfig.add("lang.afterTax") }
+            Lang.redeemSuc =
+                instance.config.getString("lang.redeemSuc") ?: "§2兑换成功".also { unFindConfig.add("lang.redeemSuc") }
+            Lang.redeemFail = instance.config.getString("lang.redeemFail")
+                ?: "§c兑换失败".also { unFindConfig.add("lang.redeemFail") }
+            Lang.cannotBuySelfGoods = instance.config.getString("lang.cannotBuySelfGoods")
+                ?: "§c你不能购买自己的商品".also { unFindConfig.add("lang.cannotBuySelfGoods") }
+            Lang.notBelongToYou = instance.config.getString("lang.notBelongToYou")
+                ?: "§c这不是你的商品".also { unFindConfig.add("lang.notBelongToYou") }
+            Lang.sellPriceError = instance.config.getString("lang.sellPriceError")
+                ?: "§c出售价格错误".also { unFindConfig.add("lang.sellPriceError") }
+            Lang.helpMessage =
+                (instance.config.getList("lang.helpMessage") ?: arrayListOf("无帮助信息")).joinToString("\n")
+            Lang.noPermission = instance.config.getString("lang.noPermission")
+                ?: "§c你没有权限".also { unFindConfig.add("lang.noPermission") }
+            Lang.reloadLang = instance.config.getString("lang.reloadLang")
+                ?: "§2重载语言文件成功".also { unFindConfig.add("lang.reloadLang") }
+            Lang.taxRateInfo = instance.config.getString("lang.taxRateInfo")
+                ?: "§2税率 §6".also { unFindConfig.add("lang.taxRateInfo") }
         } catch (e: Exception) {
+            e.printStackTrace()
             logger.warning("[UltimateInventoryShop]Config file is not correct!")
             logger.warning("[UltimateInventoryShop]配置文件读取失败")
             UltimateInventoryShopPlugin.instance.onDisable()
         }
         try {
-            UISMaterial.pageIndexMaterial = instance.config.getString("material.pageIndexMaterial")!!
-            UISMaterial.previousPageMaterial = instance.config.getString("material.previousPageMaterial")!!
-            UISMaterial.nextPageMaterial = instance.config.getString("material.nextPageMaterial")!!
-            UISMaterial.myGoods = instance.config.getString("material.myGoods")!!
-            UISMaterial.myGoodsBeenSold = instance.config.getString("material.myGoodsBeenSold")!!
-            UISMaterial.currentGoods = instance.config.getString("material.currentGoods")!!
+            UISMaterial.pageIndexMaterial = instance.config.getString("material.pageIndexMaterial")
+                ?: "PAPER".also { unFindConfig.add("material.pageIndexMaterial") }
+            UISMaterial.previousPageMaterial = instance.config.getString("material.previousPageMaterial")?: "BIRCH_BUTTON".also { unFindConfig.add("material.previousPageMaterial") }
+            UISMaterial.nextPageMaterial = instance.config.getString("material.nextPageMaterial")?: "BIRCH_BUTTON".also { unFindConfig.add("material.nextPageMaterial") }
+            UISMaterial.myGoods = instance.config.getString("material.myGoods")?: "GRASS_BLOCK".also { unFindConfig.add("material.myGoods") }
+            UISMaterial.myGoodsBeenSold = instance.config.getString("material.myGoodsBeenSold")?: "GOLD_INGOT".also { unFindConfig.add("material.myGoodsBeenSold") }
+            UISMaterial.currentGoods = instance.config.getString("material.currentGoods")?: "DIAMOND".also { unFindConfig.add("material.currentGoods") }
         } catch (e: Exception) {
+            e.printStackTrace()
             logger.warning("${Lang.prefix}${Lang.materialLoadError}")
+            if (unFindConfig.isNotEmpty()) {
+                instance.logger.warning("未找到以下配置项(Unable to find these config): ${unFindConfig.joinToString(", ")}")
+            }
         }
     }
 }
